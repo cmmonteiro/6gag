931d9f16e08271a1980c1f7ea7f213b7bf05284e
cmonteiro <cmonteiro@cdt.com.ar>
2018-08-03 15:13:48 -0300

    pagina para cargar fotos

---
+++ src/pages/loadpothos/loadpothos.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-loadpothos',
+  templateUrl: 'loadpothos.html',
+})
+export class LoadpothosPage {
+
+  constructor() {
+  }
+
+ 
+}
