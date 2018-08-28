5c38548296aa7bc252d971645b99cd3559afa0e4
Carina Monteiro <cmonteiro@cdt.com.ar>
2018-08-28 18:20:48 -0300

    fix sacar foto, no muestra el preview- why?

--- src/pages/loadpothos/loadpothos.ts
+++ src/pages/loadpothos/loadpothos.ts
@@ -14,7 +14,7 @@ export class LoadpothosPage {
   
 
   titulo:string;
-  imagenPreview:String;
+  imagenPreview :string;
   imagen64 : string;
   
   constructor(private viewCtrl: ViewController, private camera: Camera, private imagePicker: ImagePicker, public _cap:LoadFileProvider) {
@@ -27,7 +27,6 @@ export class LoadpothosPage {
   }
 
   show_camera(){
-
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
@@ -35,15 +34,17 @@ export class LoadpothosPage {
       mediaType: this.camera.MediaType.PICTURE
     }
 
-    this.camera.getPicture(options).then((imageData) => {
-    // imageData is either a base64 encoded string or a file URI
-    // If it's base64 (DATA_URL):
-    this.imagenPreview ='data:image/jpeg;base64,' + imageData;
-    this.imagen64 = imageData;
-    }, (err) => {
-        console.log("Error en camara",JSON.stringify(err));
-    });
-  }
+    this.camera.getPicture(options)
+        .then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            this.imagenPreview ='data:image/jpeg;base64,' + imageData;
+            //this.imagen64 = imageData;
+        }
+        ,(err) => {
+            console.log("Error en camara",JSON.stringify(err));
+        });
+   }
 
   select_picture(){
 
@@ -54,8 +55,6 @@ export class LoadpothosPage {
       maximumImagesCount:1
 
     }
-
-
     this.imagePicker.getPictures(opciones).then((results) => {
       for (var i = 0; i < results.length; i++) {
         this.imagenPreview ='data:image/jpeg;base64,' +  results[i];
@@ -69,13 +68,11 @@ export class LoadpothosPage {
 
 
   crear_post(){
-
     let archivo = {
       img : this.imagen64,
       titulo:this.titulo
 
     }
-
     this._cap.load_images_firebase(archivo);
 
   }
